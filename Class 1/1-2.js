61437505a3f9c72e3b36f4dde60beea44e6c4753
cjcapone85 <99698474+cjcapone85@users.noreply.github.com>
2022-03-14 18:19:23 -0500

    updates

---
+++ Class 1/1-2.js
@@ -0,0 +1,16 @@
+//const header= document.querySelector('h1');
+//header.style.color= 'green';
+ 
+//onclick=header.style.color='green';
+function background_color_change_function() {
+    document.getElementById("button").style.color = "red";
+  }
+
+// function colorChanger(color) {
+//     document.querySelector('h1').style.color = 'red'
+//     //document.body.style.background = color;
+//     onclick="background_color_change_function()"
+// }
+
+
+// **Aim**: Create a new project (with an HTML and JavaScript) that uses JavaScript to change the color of a Heading element on a button click.
